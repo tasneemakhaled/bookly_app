3e6c565ed336b7d64597472ca1baab2217014b22
tasneemakhaled <khaledtasneem402@gmail.com>
2026-01-22 01:24:44 +0200

    add all styles of project

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xff100B20);
+const kTranstionDuration = Duration(milliseconds: 250);
+const kGtSectraFine = 'GT Sectra Fine';
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view_item.dart';
@@ -8,11 +9,16 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
-      children: [
-        CustomAppBar(),
-        FeaturedListView(),
-      ],
+    return  Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedListView(),
+          Text('Best Seller',style: Styles.textStyle18,)
+        ],
+      ),
     );
   }
 }
