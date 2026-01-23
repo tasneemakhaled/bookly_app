89b7e15bf774953b3224a872cc354102c7bf613d
tasneemakhaled <khaledtasneem402@gmail.com>
2026-01-23 22:51:30 +0200

    add title, subTitle and rating

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,6 @@
+import 'package:bookly_app/core/utils/constants.dart';
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +20,15 @@ class BookDetailsViewBody extends StatelessWidget {
        Padding(
          padding: EdgeInsets.symmetric(horizontal: width*.17),
          child: CustomBookImage(),
-       )
+       ),
+        Text('''The Jungle Book''',style: Styles.textStyle30.copyWith( fontFamily: kGtSectraFine,),maxLines: 2,),
+        SizedBox(height: 7,),
+        Opacity(
+          opacity: .7,
+          child: Text('Rodyard Kipling',style:Styles.textStyle18)), 
+          SizedBox(height: 10,) ,   
+           BookRating(mainAxisAlignment: MainAxisAlignment.center,),
+          
       ],),
     );
   }
--- lib/features/home/presentation/views/widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -2,17 +2,20 @@ import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating({super.key,  this.mainAxisAlignment=MainAxisAlignment.start});
+final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return  Row(
+      mainAxisAlignment: mainAxisAlignment,
             children:[
-        Icon(FontAwesomeIcons.solidStar,color:Color(0xffFFDD4F)),
-        SizedBox(width:5)
+        Icon(FontAwesomeIcons.solidStar,color:Color(0xffFFDD4F),size: 14,),
+        SizedBox(width:7)
 ,            Text('4.8',style:Styles.textStyle16),
-SizedBox(width:5),
-            Text('(250)',style:Styles.textStyle14.copyWith(color:Color(0xff707070))),
+SizedBox(width:7),
+            Opacity(
+              opacity: .5,
+              child: Text('(250)',style:Styles.textStyle14)),
           ])  
            ;
           
