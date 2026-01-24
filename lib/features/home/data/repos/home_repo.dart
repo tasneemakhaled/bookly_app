7985fd93fad66dde9bc018efea9394a448e5f4a1
tasneemakhaled <khaledtasneem402@gmail.com>
2026-01-25 00:45:29 +0200

    either class done

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/core/utils/errors/failure.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo{  // repository pattern  بحدد الميثودس اللي هتتنفذ ميهمنيش ازاي 
-  fetchFaeturedBokks();
-  fetchBestSellerBooks();
+Future<Either <Failures,List<BookModel>>>  fetchFaeturedBokks();
+ Future <Either<Failures,List<BookModel>>>  fetchBestSellerBooks();
 }
