108a50dd2295a1c1bddcc64e1f679468b3e87461
tasneemakhaled <khaledtasneem402@gmail.com>
2026-01-25 17:53:51 +0200

    featured and newest cubit done

--- lib/features/home/presentation/view_models/cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/view_models/cubit/featured_books_cubit.dart
@@ -1,11 +1,21 @@
 
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
-  
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void>fetchFeaturedBooks()async{
+    emit(FeaturedBooksLoading());
+    var res=await homeRepo.fetchFaeturedBokks();
+    res.fold((failure){
+      emit(FeaturedBooksFailure(errorMessage: failure.errorMessage));
+    }, (success){
+      emit(FeaturedBooksSuccess(books: success));
+    });
+  }
 }
--- lib/features/home/presentation/view_models/cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/view_models/cubit/newest_books_cubit.dart
@@ -1,10 +1,21 @@
 
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void>fetchNewestBooks()async{
+    emit(NewestBooksLoading());
+    var res=await homeRepo.fetchNewestBooks();
+res.fold((failure){
+  emit(NewestBooksFailure(errorMessage: failure.errorMessage));
+}, (books){
+emit(NewestBooksSuccess(books: books));
+});
+  }
 }
