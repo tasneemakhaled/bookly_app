7180a3f9dd3643c108e75ff741946c2c0aa7a745
tasneemakhaled <khaledtasneem402@gmail.com>
2026-01-25 17:16:33 +0200

    implement fetch featured books

--- lib/features/home/data/repos/homr_repo_impl.dart
+++ lib/features/home/data/repos/homr_repo_impl.dart
@@ -11,7 +11,7 @@ class HomrRepoImpl implements HomeRepo{
   @override
   Future<Either<Failures, List<BookModel>>> fetchNewestBooks() async{
    try {
-  var data= await apiService.get(endPonit: '?filter=free-ebooks&q=Sport&Sorting=newest');
+  var data= await apiService.get(endPonit: '?filter=free-ebooks&q=Programming&Sorting=newest');
   List<BookModel>books=[];
   for (var item in data['items']) {
     books.add(BookModel.fromJson(item));
@@ -26,9 +26,21 @@ return left(ServerFailure(e.toString()));
   }
 
   @override
-  Future<Either<Failures, List<BookModel>>> fetchFaeturedBokks() {
-    // TODO: implement fetchFaeturedBokks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchFaeturedBokks()async {
+    
+  try {
+  var data= await apiService.get(endPonit: '?filter=free-ebooks&q=Programming');
+  List<BookModel>books=[];
+  for (var item in data['items']) {
+    books.add(BookModel.fromJson(item));
+  }
+  return right(books);
+}catch (e) {
+ if (e is DioException) {
+  return left(ServerFailure.fromDioException(e));
+}
+return left(ServerFailure(e.toString()));
+}
   }
 
 
