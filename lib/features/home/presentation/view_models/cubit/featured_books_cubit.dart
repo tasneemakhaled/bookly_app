fef67e92d0c40ac82e719f7f247fc358ce4cd7c6
tasneemakhaled <khaledtasneem402@gmail.com>
2026-01-25 17:44:49 +0200

    create cubits and states for fatured and newest books

---
+++ lib/features/home/presentation/view_models/cubit/featured_books_cubit.dart
@@ -0,0 +1,11 @@
+
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  
+}
---
+++ lib/features/home/presentation/view_models/cubit/featured_books_state.dart
@@ -0,0 +1,21 @@
+part of 'featured_books_cubit.dart';
+
+sealed class FeaturedBooksState extends Equatable {
+  const FeaturedBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class FeaturedBooksInitial extends FeaturedBooksState {}
+final class FeaturedBooksLoading extends FeaturedBooksState {}
+final class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookModel> books;
+
+ const  FeaturedBooksSuccess({required this.books});
+}
+final class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errorMessage;
+
+ const FeaturedBooksFailure({required this.errorMessage});
+}
